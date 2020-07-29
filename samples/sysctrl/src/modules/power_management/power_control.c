d0a419d0831f48088e7d5c86ea3f94ab889b1298
Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>
2020-07-29 13:50:06 +0200

    samples: sysctrl: remove legacy events
    Those are not used anymore due to changes in architecture.
    
    Signed-off-by: Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>

--- samples/sysctrl/src/modules/power_management/power_control.c
+++ samples/sysctrl/src/modules/power_management/power_control.c
@@ -12,8 +12,6 @@ LOG_MODULE_DECLARE(MAIN);
 #include "status_event.h"
 #include "prism_event.h"
 #include "clock_event.h"
-#include "sleep_event.h"
-#include "performance_event.h"
 #include "radio_event.h"
 #include "gpms_event.h"
 #include "gpms_notify_event.h"
@@ -27,11 +25,6 @@ LOG_MODULE_DECLARE(MAIN);
 
 #include <nrfs_gpms.h>
 
-static struct {
-	uint32_t dummy_service_specific_data;
-	struct ncm_ctx ctx;
-} power_control_ctx[1]; /* TODO: should be probably defined via Kconfig */
-
 static inline bool is_gpms_fault(int32_t gpms_status)
 {
 	return (bool) gpms_status;
@@ -41,17 +34,6 @@ static void power_control_init(void)
 {
 	pm_pcm_dispatcher_init();
 	pm_pcm_scheduler_init();
-}
-
-static void power_handle(struct pm_request_power *pwr)
-{
-	struct pm_return ret = pm_pcm_dispatcher_event_request(PM_PCM_DISPATCHER_EVENT_POWER, pwr);
-
-	if (ret.response != PM_RESPONSE_NONE) {
-		void *p_buffer = ncm_alloc(sizeof(ret));
-		(void)memcpy(p_buffer, &ret, sizeof(ret));
-		ncm_notify(&power_control_ctx[0].ctx, p_buffer, sizeof(ret));
-	}
 }
 
 /* @brief Function for notifying GPMS.
@@ -122,63 +104,6 @@ static void clock_handle(const struct clock_event *evt)
 	prism_event_release(evt->p_msg);
 }
 
-static void sleep_handle(const struct sleep_event *evt)
-{
-	nrfs_gpms_sleep_t *p_req = (nrfs_gpms_sleep_t *)evt->p_msg->p_buffer;
-	int32_t result;
-
-	LOG_DBG("Got off, domain = %d, time = %08x%08x",
-		evt->p_msg->domain_id,
-		(uint32_t)((p_req->data.time & 0xFFFFFFFF00000000) >> 32),
-		(uint32_t)(p_req->data.time & 0x00000000FFFFFFFF));
-
-	/* Check if GPMS failed or not. */
-	if (is_gpms_fault(evt->status)) {
-		/* GPMS encountered severe error. Rewrite error code
-		 * to the sleep structure and return response.
-		 * Or assign other error code - TBD. */
-		result = evt->status;
-	} else {
-		/* TODO */
-		struct pm_request_sleep sleep_req;
-
-		sleep_req.domain = evt->p_msg->domain_id;
-		sleep_req.state = (enum pm_sleep)p_req->data.state;
-		sleep_req.time = p_req->data.time;
-		sleep_req.latency = p_req->data.latency;
-
-		/* Dummy response. */
-		result = 0;
-
-	}
-
-	/* Operation is done. We can handle response if needed. */
-	nrfs_gpms_sleep_rsp_data_t gpms_sleep_res_data;
-	uint32_t msg_ctx = (uint32_t)p_req->ctx.ctx;
-
-	gpms_sleep_res_data.response = result;
-	if (pm_notify(&gpms_sleep_res_data,
-		      sizeof(gpms_sleep_res_data),
-		      msg_ctx,
-		      result) != 0) {
-		__ASSERT(false, "Failed to allocate memory for the sleep response data.");
-	}
-	prism_event_release(evt->p_msg);
-}
-
-static void performance_handle(struct performance_event *evt)
-{
-	LOG_DBG("Got performance, domain = %d, time = %08x%08x",
-		evt->request.domain,
-		(uint32_t)((evt->request.time & 0xFFFFFFFF00000000) >> 32),
-		(uint32_t)(evt->request.time & 0x00000000FFFFFFFF));
-
-	/* Transform pm_request_performance into pm_request_power */
-	struct pm_request_power req = pm_pcm_performance_into_power(&evt->request);
-
-	power_handle(&req);
-}
-
 static void radio_handle(const struct radio_event *evt)
 {
 	nrfs_gpms_radio_t *p_req = (nrfs_gpms_radio_t *)evt->p_msg->p_buffer;
@@ -233,20 +158,6 @@ static bool event_handler(const struct event_header *eh)
 		return true;
 	}
 
-	if (is_sleep_event(eh)) {
-		const struct sleep_event *evt = cast_sleep_event(eh);
-
-		sleep_handle(evt);
-		return true;
-	}
-
-	if (is_performance_event(eh)) {
-		struct performance_event *evt = cast_performance_event(eh);
-
-		performance_handle(evt);
-		return true;
-	}
-
 	if (is_radio_event(eh)) {
 		const struct radio_event *evt = cast_radio_event(eh);
 
@@ -262,6 +173,4 @@ static bool event_handler(const struct event_header *eh)
 EVENT_LISTENER(POWER_CONTROL, event_handler);
 EVENT_SUBSCRIBE(POWER_CONTROL, status_event);
 EVENT_SUBSCRIBE(POWER_CONTROL, clock_event);
-EVENT_SUBSCRIBE(POWER_CONTROL, sleep_event);
-EVENT_SUBSCRIBE(POWER_CONTROL, performance_event);
 EVENT_SUBSCRIBE(POWER_CONTROL, radio_event);
