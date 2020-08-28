e2d08387fdb33708e8a068d02c602f35bcb3e1b5
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2020-08-28 11:29:14 +0200

    samples: sysctrl: integrate nrfs filter error

--- samples/sysctrl/src/modules/messaging/filter.c
+++ samples/sysctrl/src/modules/messaging/filter.c
@@ -12,31 +12,57 @@ LOG_MODULE_DECLARE(MAIN);
 #include "prism_event.h"
 #include "led_event.h"
 #include "gpms_event.h"
+#include "ncm.h"
 
 #include <internal/nrfs_hdr.h>
+
+enum filter_result {
+	FILTER_OK,
+	FILTER_SERVICE_UNRECOGNIZED,
+	FILTER_REQUEST_UNRECOGNIZED,
+	FILTER_REQUEST_REJECTED,
+};
 
 static void filter_init(void)
 {
 	LOG_INF("Filter initialized");
 }
 
-static void *led_msg_filter(nrfs_phy_t *p_msg)
+static void msg_reject(nrfs_phy_t *p_msg)
 {
-	struct led_event *led_evt = new_led_event();
+	struct ncm_ctx ctx;
 
-	led_evt->p_msg = p_msg;
-	return led_evt;
+	ncm_fill(&ctx, p_msg);
+	NRFS_HDR_FILTER_ERR_SET(&ctx.hdr);
+	void *buffer = ncm_alloc(0);
+	ncm_notify(&ctx, buffer, 0);
 }
 
-static void *gpms_msg_filter(nrfs_phy_t *p_msg)
+static enum filter_result led_msg_filter(nrfs_phy_t *p_msg, void **pp_evt)
 {
-    struct gpms_event *gpms_evt = new_gpms_event();
+	/* Simulate events filtering*/
+	static uint8_t cnt = 0;
 
-    gpms_evt->p_msg = p_msg;
-    return gpms_evt;
+	if (cnt++ % 2) {
+		struct led_event *led_evt = new_led_event();
+		led_evt->p_msg = p_msg;
+		*pp_evt = led_evt;
+		return FILTER_OK;
+	} else   {
+		return FILTER_REQUEST_REJECTED;
+	}
 }
 
-static void * (*const filters[])(nrfs_phy_t * p_msg) =
+static enum filter_result gpms_msg_filter(nrfs_phy_t *p_msg, void **pp_evt)
+{
+	struct gpms_event *gpms_evt = new_gpms_event();
+
+	gpms_evt->p_msg = p_msg;
+	*pp_evt = gpms_evt;
+	return FILTER_OK;
+}
+
+static enum filter_result(*const filters[])(nrfs_phy_t * p_msg, void **pp_evt) =
 {
 	[NRFS_SERVICE_ID_LED] = led_msg_filter,
 	[NRFS_SERVICE_ID_GPMS] = gpms_msg_filter,
@@ -51,19 +77,32 @@ static void msg_received(struct prism_event *evt)
 	uint8_t srv_id = NRFS_SERVICE_ID_GET(p_hdr->req);
 
 	void *p_evt = NULL;
+	enum filter_result result = FILTER_SERVICE_UNRECOGNIZED;
 	if (srv_id < ARRAY_SIZE(filters)) {
-		p_evt = filters[srv_id](evt->p_msg);
+		result = filters[srv_id](evt->p_msg, &p_evt);
 	}
 
-	if (!p_evt) {
-		p_evt = new_prism_event();
-		struct prism_event *prism_evt = cast_prism_event(p_evt);
-		prism_evt->p_msg = evt->p_msg;
-		prism_evt->status = PRISM_MSG_STATUS_RX_RELEASED;
-		LOG_ERR("Request type could not be determined: %u", p_hdr->req);
+	switch (result) {
+	case FILTER_OK:
+		_event_submit(p_evt);
+		break;
+	case FILTER_SERVICE_UNRECOGNIZED:
+		LOG_ERR("Service unrecognized: %u", srv_id);
+		break;
+	case FILTER_REQUEST_UNRECOGNIZED:
+		LOG_ERR("Request unrecognized: %u", p_hdr->req);
+		break;
+	case FILTER_REQUEST_REJECTED:
+		LOG_ERR("Request rejected: %u", p_hdr->req);
+		msg_reject(evt->p_msg);
+		break;
+	default:
+		break;
 	}
 
-	_event_submit(p_evt);
+	if (result != FILTER_OK) {
+		prism_event_release(evt->p_msg);
+	}
 }
 
 static bool event_handler(const struct event_header *eh)
