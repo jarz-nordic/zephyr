24a7f0149b109a475e16bf4daf73c69ebb408c7c
Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>
2020-09-03 09:33:35 +0200

    HFCLK tests
    Signed-off-by: Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>

--- samples/sysctrl/src/main.c
+++ samples/sysctrl/src/main.c
@@ -1,17 +1,13 @@
-/*
- * Copyright (c) 2019 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: Apache-2.0
- */
 
 #include <zephyr.h>
-
 #include <hal/nrf_timer.h>
-#include <hal/nrf_dppi.h>
 #include <hal/nrf_gpio.h>
 #include <hal/nrf_gpiote.h>
-#include <hal/nrf_rtc.h>
-
+#include <hal/nrf_dppi.h>
+#include <hal/nrf_clock.h>
+#include <compiler_abstraction.h>
+#include <drivers/clock_control/nrf_clock_control.h>
+#include <drivers/clock_control.h>
 #include <prism_dispatcher.h>
 #include <event_manager.h>
 #include <status_event.h>
@@ -19,55 +15,33 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(MAIN, LOG_LEVEL_INF);
 
+#define CONSOLE_LABEL DT_LABEL(DT_CHOSEN(zephyr_console))
 
-#define AUX_PIN		12
-#define DPPI_PIN	9
+#define AUX_PIN			12
+#define DPPI_PIN		9
+#define SYSCTL_TIMER 	NRF_TIMER1
+#define MY_DPPI_EVENT	NRF_RTC_EVENT_TICK
+#define WAIT_TICKS		(32768 * 4)
+#define GPIOTE			NRF_GPIOTE
 
-#define LOCAL_DOMAIN_TIMER_FREQUENCY_Hz	1000000
+void TIMER1_IRQHandler(void *arg)
+{
+    //nrf_gpio_pin_clear(AUX_PIN);
+    if (nrf_timer_event_check(SYSCTL_TIMER, NRF_TIMER_EVENT_COMPARE1)) {
+        nrf_timer_event_clear(SYSCTL_TIMER, NRF_TIMER_EVENT_COMPARE1);
+        nrf_gpiote_task_enable(GPIOTE, 0);
+        //printk("XXXXXXX");
+    }
+    //nrf_gpio_pin_set(AUX_PIN);
+}
 
-
-static volatile bool trg;
 static void backdoor_cb(uint64_t *value)
-{
-	nrf_gpiote_task_enable(NRF_GPIOTE0, 0);
-	nrf_gpio_pin_clear(AUX_PIN);
-	uint64_t now = (uint64_t)nrf_rtc_counter_get(NRF_RTC1) + 1;
-	nrf_rtc_event_clear(NRF_RTC1, NRF_RTC_EVENT_TICK);
-	nrf_rtc_event_enable(NRF_RTC1, NRF_RTC_INT_TICK_MASK);
-	if (nrf_rtc_event_check(NRF_RTC1, NRF_RTC_EVENT_TICK)) {
-		now = (uint64_t)nrf_rtc_counter_get(NRF_RTC1);
-		nrf_rtc_event_disable(NRF_RTC1, NRF_RTC_INT_TICK_MASK);
-		nrf_rtc_event_clear(NRF_RTC1, NRF_RTC_EVENT_TICK);
-	}
-	else {
-		nrf_rtc_int_enable(NRF_RTC1, NRF_RTC_INT_TICK_MASK);
-	}
-	*value = ((now * LOCAL_DOMAIN_TIMER_FREQUENCY_Hz) / 32768);
-	nrf_gpio_pin_set(AUX_PIN);
-	LOG_INF("Time request: %u (now=%u)", (uint32_t)*value, (uint32_t)now - 1);
-	trg = true;
-
-}
-
-static void timer_channel_init(void)
-{
-	nrf_dppi_channels_disable_all(NRF_DPPIC);
-	nrf_gpio_cfg_output(AUX_PIN);
-	nrf_gpio_pin_set(AUX_PIN);
-
-	nrf_gpiote_task_disable(NRF_GPIOTE0, 0);
-	nrf_gpiote_subscribe_set(NRF_GPIOTE0, NRF_GPIOTE_TASK_SET_0, 0);
-	nrf_gpiote_task_configure(NRF_GPIOTE0, 0, DPPI_PIN, GPIOTE_CONFIG_POLARITY_LoToHi, NRF_GPIOTE_INITIAL_VALUE_LOW);
-	nrf_gpiote_task_enable(NRF_GPIOTE0, 0);
-
-	nrf_dppi_channels_enable(NRF_DPPIC, 1);
-
-	nrf_gpio_pin_mcu_select(11, NRF_GPIO_PIN_MCUSEL_NETWORK);
-	nrf_gpio_pin_mcu_select(23, NRF_GPIO_PIN_MCUSEL_NETWORK);
-}
+{}
 
 void main(void)
 {
+    //nrf_clock_event_clear(NRF_CLOCK, NRF_CLOCK_EVENT_HFCLKSTARTED);
+    //nrf_clock_task_trigger(NRF_CLOCK, NRF_CLOCK_TASK_HFCLKSTART);
 	if (event_manager_init()) {
 		LOG_ERR("Event Manager not initialized");
 	} else {
@@ -76,12 +50,44 @@ void main(void)
 		EVENT_SUBMIT(event);
 	}
 	prism_dispatcher_backdoor_register(backdoor_cb);
-	timer_channel_init();
-	while(1) {
-		LOG_INF("CT:%u %x", (uint32_t)z_timer_cycle_get_32(), NRF_RTC1->PUBLISH_TICK);
 
-		while (!trg) k_sleep(K_MSEC(1));
-		trg = false;
+	//nrf_802154_clock_hfclk_start();
+	nrf_gpio_cfg_output(AUX_PIN);
+	nrf_gpio_pin_clear(AUX_PIN);
+	nrf_gpio_cfg_output(DPPI_PIN);
+	nrf_gpio_pin_clear(DPPI_PIN);
+	//nrf_gpio_cfg_output(11);
+	//nrf_gpio_pin_clear(11);
+	//nrf_gpio_cfg_output(8);
+	//nrf_gpio_pin_clear(8);
 
+	nrf_timer_frequency_set(SYSCTL_TIMER, NRF_TIMER_FREQ_1MHz);
+	nrf_timer_cc_set(SYSCTL_TIMER, NRF_TIMER_CC_CHANNEL1, 100);
+	nrf_timer_bit_width_set(SYSCTL_TIMER, NRF_TIMER_BIT_WIDTH_32);
+	nrf_timer_int_enable(SYSCTL_TIMER, TIMER_INTENSET_COMPARE1_Msk);
+	//nrf_timer_shorts_enable(SYSCTL_TIMER, 0x0200);
+	nrf_timer_shorts_enable(SYSCTL_TIMER, NRF_TIMER_SHORT_COMPARE1_CLEAR_MASK);
+	IRQ_CONNECT(TIMER1_IRQn, 1, TIMER1_IRQHandler, 0, 0);
+	irq_enable(TIMER1_IRQn);
+	nrf_timer_publish_set(SYSCTL_TIMER, NRF_TIMER_EVENT_COMPARE1, 0);
+	nrf_timer_task_trigger(SYSCTL_TIMER, NRF_TIMER_TASK_START);
+
+	nrf_gpiote_task_disable(GPIOTE, 0);
+	nrf_gpiote_subscribe_set(GPIOTE, NRF_GPIOTE_TASK_SET_0, 0);
+	nrf_gpiote_task_configure(GPIOTE, 0, DPPI_PIN, GPIOTE_CONFIG_POLARITY_LoToHi, NRF_GPIOTE_INITIAL_VALUE_LOW);
+	nrf_gpiote_task_enable(GPIOTE, 0);
+
+	nrf_dppi_channels_enable(NRF_DPPIC, 1);
+	nrf_gpio_pin_mcu_select(11, NRF_GPIO_PIN_MCUSEL_NETWORK);
+	nrf_gpio_pin_mcu_select(23, NRF_GPIO_PIN_MCUSEL_NETWORK);
+
+	//nrf_rtc_task_trigger(RTC, NRF_RTC_TASK_START);
+
+	while(1)
+	{
+		nrf_timer_task_trigger(SYSCTL_TIMER, NRF_TIMER_TASK_CAPTURE0);
+		printk("CT:%u %x\n", (uint32_t)SYSCTL_TIMER->CC[0], NRF_CLOCK->HFCLKSTAT);
+		k_sleep(K_MSEC(1000));
+		//nrf_gpiote_task_trigger(GPIOTE, NRF_GPIOTE_TASK_SET_0);
 	}
 }
--- samples/sysctrl_local_domain/main.c
+++ samples/sysctrl_local_domain/main.c
@@ -1,40 +1,35 @@
-/*
- * Copyright (c) 2019 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: Apache-2.0
- */
 
 #include <zephyr.h>
-
 #include <hal/nrf_timer.h>
-#include <hal/nrf_dppi.h>
 #include <hal/nrf_gpio.h>
 #include <hal/nrf_gpiote.h>
-
+#include <hal/nrf_dppi.h>
+#include <hal/nrf_clock.h>
+#include <compiler_abstraction.h>
+#include <drivers/clock_control/nrf_clock_control.h>
+#include <drivers/clock_control.h>
 #include <prism_dispatcher.h>
 
 #include <logging/log.h>
 LOG_MODULE_REGISTER(MAIN, LOG_LEVEL_INF);
 
-#define DPPI_PIN	23
-#define AUX_PIN		11
-#define p_reg 		NRF_GPIOTE
+#define CONSOLE_LABEL DT_LABEL(DT_CHOSEN(zephyr_console))
 
-#define LOCAL_DOMAIN_TIMER NRF_TIMER0
+#define AUX_PIN			23
+#define DPPI_PIN		11
+#define SYSCTL_TIMER 	NRF_TIMER1
+#define MY_DPPI_EVENT	NRF_RTC_EVENT_TICK
+#define WAIT_TICKS		(32768 * 4)
+#define GPIOTE			NRF_GPIOTE
 
-static volatile uint64_t curr_time;
-
-static void GPIOTE_IRQHandler(void *arg)
+void TIMER1_IRQHandler(void *arg)
 {
-	ARG_UNUSED(arg);
-	nrf_gpiote_event_clear(NRF_GPIOTE, NRF_GPIOTE_EVENT_IN_0);
-	nrf_gpiote_event_disable(NRF_GPIOTE, 0);
-}
-
-static inline uint64_t get_counter(void)
-{
-	nrf_timer_task_trigger(LOCAL_DOMAIN_TIMER, NRF_TIMER_TASK_CAPTURE5);
-	return (uint64_t)nrf_timer_cc_get(LOCAL_DOMAIN_TIMER, NRF_TIMER_CC_CHANNEL5);
+    //nrf_gpio_pin_clear(AUX_PIN);
+    if (nrf_timer_event_check(SYSCTL_TIMER, NRF_TIMER_EVENT_COMPARE1)) {
+        nrf_timer_event_clear(SYSCTL_TIMER, NRF_TIMER_EVENT_COMPARE1);
+        nrf_gpiote_task_enable(GPIOTE, 0);
+    }
+    //nrf_gpio_pin_set(AUX_PIN);
 }
 
 void prism_irq_handler(void)
@@ -58,66 +53,55 @@ static const prism_dispatcher_ept_t m_epts[] = {
 	{ PRISM_DOMAIN_SYSCTRL, 3, eptx_handler },
 };
 
-static void timer_channel_init(void)
-{
-	nrf_gpio_cfg_output(AUX_PIN);
-	nrf_timer_event_clear(LOCAL_DOMAIN_TIMER, NRF_TIMER_EVENT_COMPARE0);
-	nrf_timer_int_enable(LOCAL_DOMAIN_TIMER, TIMER_INTENSET_COMPARE0_Msk);
-	nrf_timer_subscribe_set(LOCAL_DOMAIN_TIMER, NRF_TIMER_TASK_START, 0);
-	nrf_timer_subscribe_set(LOCAL_DOMAIN_TIMER, NRF_TIMER_TASK_CAPTURE0, 0);
-	nrf_timer_bit_width_set(LOCAL_DOMAIN_TIMER, NRF_TIMER_BIT_WIDTH_32);
-	nrf_timer_frequency_set(LOCAL_DOMAIN_TIMER, NRF_TIMER_FREQ_1MHz);
-
-	nrf_gpiote_event_configure(NRF_GPIOTE, 0, DPPI_PIN, NRF_GPIOTE_POLARITY_LOTOHI);
-	nrf_gpiote_publish_set(NRF_GPIOTE, NRF_GPIOTE_EVENT_IN_0, 0);
-
-	nrf_gpiote_event_enable(NRF_GPIOTE, 0);
-
-	nrf_dppi_channels_enable(NRF_DPPIC, 1);
-
-	//IRQ_CONNECT(TIMER0_IRQn, 1, TIMER0_IRQHandler, 0, 0);
-	//irq_enable(TIMER0_IRQn);
-	nrf_gpio_pin_set(AUX_PIN);
-}
-
-static void synchronize_timers(void)
-{
-	nrf_gpio_pin_clear(AUX_PIN);
-	uint64_t req = 0xFFFFFFFF;
-
-	nrf_timer_task_trigger(LOCAL_DOMAIN_TIMER, NRF_TIMER_TASK_STOP);
-	nrf_timer_task_trigger(LOCAL_DOMAIN_TIMER, NRF_TIMER_TASK_CLEAR);
-	nrf_gpiote_event_enable(NRF_GPIOTE, 0);
-
-	//SyncRequest
-	prism_dispatcher_backdoor_xfer(&req);
-	curr_time = req;
-
-
-	while(!nrf_gpiote_event_check(NRF_GPIOTE, NRF_GPIOTE_EVENT_IN_0));
-	nrf_gpio_pin_set(AUX_PIN);
-	nrf_gpiote_event_disable(NRF_GPIOTE, 0);
-	nrf_gpiote_event_clear(NRF_GPIOTE, NRF_GPIOTE_EVENT_IN_0);
-	nrf_gpio_pin_set(AUX_PIN);
-	LOG_INF("CTR:%u", (uint32_t)curr_time);
-}
-
 void main(void)
 {
+    nrf_clock_event_clear(NRF_CLOCK, NRF_CLOCK_EVENT_HFCLKSTARTED);
+    nrf_clock_task_trigger(NRF_CLOCK, NRF_CLOCK_TASK_HFCLKSTART);
+	//nrf_802154_clock_hfclk_start();
+
 	prism_dispatcher_err_t status;
 
 	status = prism_dispatcher_init(prism_irq_handler, m_epts,
 				       ARRAY_SIZE(m_epts));
 	if (status != PRISM_DISPATCHER_OK) {
-		LOG_ERR("Dispatcher init failed: %d", status);
+		printk("Dispatcher init failed: %d", status);
 	}
-	timer_channel_init();
-	k_sleep(K_MSEC(1000));
+	nrf_gpio_cfg_output(AUX_PIN);
+	nrf_gpio_pin_set(AUX_PIN);
+	nrf_gpio_cfg_output(DPPI_PIN);
+	nrf_gpio_pin_clear(DPPI_PIN);
+	//nrf_gpio_cfg_output(11);
+	//nrf_gpio_pin_clear(11);
+	//nrf_gpio_cfg_output(8);
+	//nrf_gpio_pin_clear(8);
 
-	synchronize_timers();
+	nrf_timer_frequency_set(SYSCTL_TIMER, NRF_TIMER_FREQ_1MHz);
+	nrf_timer_cc_set(SYSCTL_TIMER, NRF_TIMER_CC_CHANNEL1, 100);
+	nrf_timer_bit_width_set(SYSCTL_TIMER, NRF_TIMER_BIT_WIDTH_32);
+	nrf_timer_int_enable(SYSCTL_TIMER, TIMER_INTENSET_COMPARE1_Msk);
+	//nrf_timer_shorts_enable(SYSCTL_TIMER, 0x0200);
+	nrf_timer_shorts_enable(SYSCTL_TIMER, NRF_TIMER_SHORT_COMPARE1_CLEAR_MASK);
+	IRQ_CONNECT(TIMER1_IRQn, 1, TIMER1_IRQHandler, 0, 0);
+	irq_enable(TIMER1_IRQn);
+	nrf_timer_publish_set(SYSCTL_TIMER, NRF_TIMER_EVENT_COMPARE1, 0);
+	nrf_timer_task_trigger(SYSCTL_TIMER, NRF_TIMER_TASK_START);
 
-	while(1) {
+	nrf_gpiote_task_disable(GPIOTE, 0);
+	nrf_gpiote_subscribe_set(GPIOTE, NRF_GPIOTE_TASK_SET_0, 0);
+	nrf_gpiote_task_configure(GPIOTE, 0, DPPI_PIN, GPIOTE_CONFIG_POLARITY_LoToHi, NRF_GPIOTE_INITIAL_VALUE_LOW);
+	nrf_gpiote_task_enable(GPIOTE, 0);
+
+	nrf_dppi_channels_enable(NRF_DPPIC, 1);
+	//nrf_gpio_pin_mcu_select(11, NRF_GPIO_PIN_MCUSEL_NETWORK);
+	//nrf_gpio_pin_mcu_select(23, NRF_GPIO_PIN_MCUSEL_NETWORK);
+
+	//nrf_rtc_task_trigger(RTC, NRF_RTC_TASK_START);
+
+	while(1)
+	{
+		nrf_timer_task_trigger(SYSCTL_TIMER, NRF_TIMER_TASK_CAPTURE0);
+		printk("CT:%u %x\n", (uint32_t)SYSCTL_TIMER->CC[0], NRF_CLOCK->HFCLKSTAT);
 		k_sleep(K_MSEC(1000));
-		synchronize_timers();
+		//nrf_gpiote_task_trigger(GPIOTE, NRF_GPIOTE_TASK_SET_0);
 	}
 }
