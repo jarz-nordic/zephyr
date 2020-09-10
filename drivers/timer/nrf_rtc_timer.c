21ae7ab45991635150db09a149d6d39b7a7a6a2e
Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>
2020-09-10 12:34:49 +0200

    Add NRF_TIMER0 as the system timer
    Signed-off-by: Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>

--- drivers/timer/nrf_rtc_timer.c
+++ drivers/timer/nrf_rtc_timer.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017 Nordic Semiconductor ASA
+ * Copyright (c) 2016-2020 Nordic Semiconductor ASA
  * Copyright (c) 2018 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
@@ -10,16 +10,34 @@
 #include <drivers/clock_control/nrf_clock_control.h>
 #include <drivers/timer/system_timer.h>
 #include <sys_clock.h>
-#include <hal/nrf_rtc.h>
 #include <spinlock.h>
 
-#define RTC NRF_RTC1
-#define RTC_IRQn NRFX_IRQ_NUMBER_GET(RTC)
-#define RTC_LABEL rtc1
+#include <hal/nrf_clock.h>
+#if (IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0))
+#include <hal/nrf_timer.h>
+#elif (IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1))
+#include <hal/nrf_rtc.h>
+#endif
 
+#if IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0)
+
+#define SYSTEM_TIMER NRF_TIMER0
+#define SYSTEM_TIMER_IRQn NRFX_IRQ_NUMBER_GET(SYSTEM_TIMER)
+#define SYSTEM_TIMER_LABEL timer1
+
+#define DEFAULT_FREQUENCY_SETTING NRF_TIMER_FREQ_16MHz
+
+#define COUNTER_MAX (0xFFFFFFFF)
+#define COUNTER_HALF_SPAN (BIT(31))
+#elif IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1)
+#define SYSTEM_TIMER NRF_RTC1
+#define SYSTEM_TIMER_IRQn NRFX_IRQ_NUMBER_GET(SYSTEM_TIMER)
+#define SYSTEM_TIMER_LABEL rtc1
 #define COUNTER_SPAN BIT(24)
-#define COUNTER_MAX (COUNTER_SPAN - 1U)
 #define COUNTER_HALF_SPAN (COUNTER_SPAN / 2U)
+#define COUNTER_MAX (COUNTER_SPAN - 1U)
+#endif // IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0)
+
 #define CYC_PER_TICK (sys_clock_hw_cycles_per_sec()	\
 		      / CONFIG_SYS_CLOCK_TICKS_PER_SEC)
 #define MAX_TICKS ((COUNTER_HALF_SPAN - CYC_PER_TICK) / CYC_PER_TICK)
@@ -29,6 +47,47 @@ static struct k_spinlock lock;
 
 static uint32_t last_count;
 
+#if IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0)
+static nrf_timer_frequency_t convert_frequency_value(uint32_t frequency_hz)
+{
+	switch (frequency_hz) {
+		case 16000000:
+			return NRF_TIMER_FREQ_16MHz;
+		case 8000000:
+			return NRF_TIMER_FREQ_8MHz;
+		case 4000000:
+			return NRF_TIMER_FREQ_4MHz;
+		case 2000000:
+			return NRF_TIMER_FREQ_2MHz;
+		case 1000000:
+			return NRF_TIMER_FREQ_1MHz;
+		case 500000:
+			return NRF_TIMER_FREQ_500kHz;
+		case 250000:
+			return NRF_TIMER_FREQ_250kHz;
+		case 125000:
+			return NRF_TIMER_FREQ_125kHz;
+		case 62500:
+			return NRF_TIMER_FREQ_62500Hz;
+		case 31250:
+			return NRF_TIMER_FREQ_31250Hz;
+		default:
+			__ASSERT_NO_MSG(false);
+			return DEFAULT_FREQUENCY_SETTING;
+	}
+}
+
+static int HFCLK_init(struct device *dev)
+{
+	ARG_UNUSED(dev);
+	nrf_clock_event_clear(NRF_CLOCK, NRF_CLOCK_EVENT_HFCLKSTARTED);
+	nrf_clock_task_trigger(NRF_CLOCK, NRF_CLOCK_TASK_HFCLKSTART);
+	while (!nrf_clock_hf_is_running(NRF_CLOCK, NRF_CLOCK_HFCLK_HIGH_ACCURACY)) {};
+	return 0;
+}
+SYS_INIT(HFCLK_init, PRE_KERNEL_1, 0);
+#endif
+
 static uint32_t counter_sub(uint32_t a, uint32_t b)
 {
 	return (a - b) & COUNTER_MAX;
@@ -36,37 +95,85 @@ static uint32_t counter_sub(uint32_t a, uint32_t b)
 
 static void set_comparator(uint32_t cyc)
 {
-	nrf_rtc_cc_set(RTC, 0, cyc & COUNTER_MAX);
+#if IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0)
+	nrf_timer_cc_set(SYSTEM_TIMER, 0, cyc & COUNTER_MAX);
+#elif IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1)
+	nrf_rtc_cc_set(SYSTEM_TIMER, 0, cyc & COUNTER_MAX);
+#endif
 }
 
 static uint32_t get_comparator(void)
 {
-	return nrf_rtc_cc_get(RTC, 0);
+#if IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0)
+	return nrf_timer_cc_get(SYSTEM_TIMER, 0);
+#elif IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1)
+	return nrf_rtc_cc_get(SYSTEM_TIMER, 0);
+#endif
 }
 
 static void event_clear(void)
 {
-	nrf_rtc_event_clear(RTC, NRF_RTC_EVENT_COMPARE_0);
+#if IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0)
+	nrf_timer_event_clear(SYSTEM_TIMER, NRF_TIMER_EVENT_COMPARE0);
+#elif IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1)
+	nrf_rtc_event_clear(SYSTEM_TIMER, NRF_RTC_EVENT_COMPARE_0);
+#endif
 }
 
 static void event_enable(void)
 {
-	nrf_rtc_event_enable(RTC, NRF_RTC_INT_COMPARE0_MASK);
+#if IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0)
+#elif IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1)
+	nrf_rtc_event_enable(SYSTEM_TIMER, NRF_RTC_INT_COMPARE0_MASK);
+#endif
 }
 
 static void int_disable(void)
 {
-	nrf_rtc_int_disable(RTC, NRF_RTC_INT_COMPARE0_MASK);
+#if IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0)
+	nrf_timer_int_disable(SYSTEM_TIMER, NRF_TIMER_INT_COMPARE0_MASK);
+#elif IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1)
+	nrf_rtc_int_disable(SYSTEM_TIMER, NRF_RTC_INT_COMPARE0_MASK);
+#endif
 }
 
 static void int_enable(void)
 {
-	nrf_rtc_int_enable(RTC, NRF_RTC_INT_COMPARE0_MASK);
+#if IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0)
+	nrf_timer_int_enable(SYSTEM_TIMER, NRF_TIMER_INT_COMPARE0_MASK);
+#elif IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1)
+	nrf_rtc_int_enable(SYSTEM_TIMER, NRF_RTC_INT_COMPARE0_MASK);
+#endif
 }
+
+static void task_clear(void)
+{
+#if IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0)
+	nrf_timer_task_trigger(SYSTEM_TIMER, NRF_TIMER_TASK_CLEAR);
+#elif IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1)
+	nrf_rtc_task_trigger(SYSTEM_TIMER, NRF_RTC_TASK_CLEAR);
+#endif
+}
+static void task_start(void)
+{
+#if IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0)
+	nrf_timer_task_trigger(SYSTEM_TIMER, NRF_TIMER_TASK_START);
+#elif IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1)
+	nrf_rtc_task_trigger(SYSTEM_TIMER, NRF_RTC_TASK_START);
+#endif
+}
+
+
+
 
 static uint32_t counter(void)
 {
-	return nrf_rtc_counter_get(RTC);
+#if IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0)
+	nrf_timer_task_trigger(SYSTEM_TIMER, NRF_TIMER_TASK_CAPTURE1);
+	return nrf_timer_cc_get(SYSTEM_TIMER, NRF_TIMER_CC_CHANNEL1);
+#elif IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1)
+	return nrf_rtc_counter_get(SYSTEM_TIMER);
+#endif
 }
 
 /* Function ensures that previous CC value will not set event */
@@ -87,17 +194,19 @@ static void prevent_false_prev_evt(void)
 	event_enable();
 
 	if (counter_sub(prev_val, now) == 1) {
+#if IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1)
 		k_busy_wait(15);
+#endif
 		event_clear();
 	}
 
 	/* Clear interrupt that may have fired as we were setting the
 	 * comparator.
 	 */
-	NVIC_ClearPendingIRQ(RTC_IRQn);
+	NRFX_IRQ_PENDING_CLEAR(SYSTEM_TIMER_IRQn);
 }
 
-/* If alarm is next RTC cycle from now, function attempts to adjust. If
+/* If alarm is next SYSTEM_TIMER cycle from now, function attempts to adjust. If
  * counter progresses during that time it means that 1 cycle elapsed and
  * interrupt is set pending.
  */
@@ -123,7 +232,7 @@ static void set_absolute_alarm(uint32_t abs_val)
 	uint32_t t = counter();
 
 	diff = counter_sub(abs_val, t);
-	if (diff == 1) {
+	if (diff == 1 && IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1)) {
 		handle_next_cycle_case(t);
 		return;
 	}
@@ -139,14 +248,22 @@ static void set_absolute_alarm(uint32_t abs_val)
 		/* It is possible that setting CC was interrupted and CC might
 		 * be set to COUNTER+1 value which will not generate an event.
 		 * In that case, special handling is performed (attempt to set
-		 * CC to COUNTER+2).
+		 * CC to COUNTER+2). In case of using NRF_TIMERx peripheral
+		 * some ticks must be added dependently on frequency. At the
+		 * moment we're assuming, that 1us is enough.
 		 */
-		handle_next_cycle_case(t);
+		if(IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1)) {
+			handle_next_cycle_case(t);
+		}
+		else if (IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0)) {
+			uint32_t offs = CONFIG_NRF_SYSTEM_TIMER_FREQUENCY_HZ / 1000000;
+			set_comparator(t + offs);
+		}
 	}
 }
 
 /* Sets relative alarm from any context. Function is lockless. It only
- * blocks RTC interrupt.
+ * blocks SYSTEM_TIMER interrupt.
  */
 static void set_protected_absolute_alarm(uint32_t cycles)
 {
@@ -178,7 +295,7 @@ void rtc_nrf_isr(const void *arg)
 	last_count += dticks * CYC_PER_TICK;
 
 	if (!IS_ENABLED(CONFIG_TICKLESS_KERNEL)) {
-		/* protection is not needed because we are in the RTC interrupt
+		/* protection is not needed because we are in the SYSTEM_TIMER interrupt
 		 * so it won't get preempted by the interrupt.
 		 */
 		set_absolute_alarm(last_count + CYC_PER_TICK);
@@ -197,25 +314,35 @@ int z_clock_driver_init(const struct device *device)
 			CLOCK_CONTROL_NRF_LF_START_AVAILABLE :
 			CLOCK_CONTROL_NRF_LF_START_STABLE);
 
-	/* TODO: replace with counter driver to access RTC */
-	nrf_rtc_prescaler_set(RTC, 0);
+	/* TODO: replace with counter driver to access SYSTEM_TIMER */
+#if (IS_ENABLED(CONFIG_NRF_TIME_SOURCE_TIMER0))
+	nrf_timer_bit_width_set(SYSTEM_TIMER, NRF_TIMER_BIT_WIDTH_32);
+	nrf_timer_frequency_set(SYSTEM_TIMER,
+		convert_frequency_value(CONFIG_NRF_SYSTEM_TIMER_FREQUENCY_HZ));
+
+#elif (IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1))
+	nrf_rtc_prescaler_set(SYSTEM_TIMER, 0);
+#endif
 	event_clear();
-	NVIC_ClearPendingIRQ(RTC_IRQn);
+	NRFX_IRQ_PENDING_CLEAR(SYSTEM_TIMER_IRQn);
 	int_enable();
 
-	IRQ_CONNECT(RTC_IRQn, DT_IRQ(DT_NODELABEL(RTC_LABEL), priority),
+	IRQ_CONNECT(SYSTEM_TIMER_IRQn,
+		    DT_IRQ(DT_NODELABEL(SYSTEM_TIMER_LABEL),priority),
 		    rtc_nrf_isr, 0, 0);
-	irq_enable(RTC_IRQn);
+	irq_enable(SYSTEM_TIMER_IRQn);
 
-	nrf_rtc_task_trigger(RTC, NRF_RTC_TASK_CLEAR);
-	nrf_rtc_task_trigger(RTC, NRF_RTC_TASK_START);
+	task_clear();
+	task_start();
 
 	if (!IS_ENABLED(CONFIG_TICKLESS_KERNEL)) {
 		set_comparator(counter() + CYC_PER_TICK);
 	}
 
-	z_nrf_clock_control_lf_on(mode);
 
+#if (IS_ENABLED(CONFIG_NRF_TIME_SOURCE_RTC1))
+	z_nrf_clock_control_lf_on(mode);
+#endif
 	return 0;
 }
 
