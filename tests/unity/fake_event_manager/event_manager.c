6a0a3b78d481089eb1189c2f52ef9826443836bd
Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>
2020-12-11 14:29:50 +0100

    ut: add fake_event_manager module
    Signed-off-by: Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>

---
+++ tests/unity/fake_event_manager/event_manager.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <stdio.h>
+#include "event_manager.h"
+
+void _event_submit(struct event_header *eh)
+{
+
+}
+
+int event_manager_init(void)
+{
+	return 0;
+}
