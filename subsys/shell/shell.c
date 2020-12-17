b5ec62dae656acb81b1af997d5a91d15713f0a24
Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>
2020-12-17 14:57:27 +0100

    shell: user callback for meta-key
    Signed-off-by: Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>

--- subsys/shell/shell.c
+++ subsys/shell/shell.c
@@ -827,6 +827,10 @@ static void alt_metakeys_handle(const struct shell *shell, char data)
 			shell_print_prompt_and_cmd(shell);
 		}
 	}
+
+	if (shell->ctx->metakey_callback) {
+		shell->ctx->metakey_callback(shell, data);
+	}
 }
 
 static void ctrl_metakeys_handle(const struct shell *shell, char data)
@@ -898,6 +902,10 @@ static void ctrl_metakeys_handle(const struct shell *shell, char data)
 
 	default:
 		break;
+	}
+
+	if (shell->ctx->metakey_callback) {
+		shell->ctx->metakey_callback(shell, data);
 	}
 }
 
