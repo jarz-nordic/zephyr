6183a258ba8b6260e3dd1529ac898fa29a218516
Jakub Rzeszutko <jrzeszutko@gmail.com>
2020-12-29 01:55:40 +0100

    piny nie dzialaja

--- samples/subsys/mgmt/mcumgr/strzelnica/src/buttons.c
+++ samples/subsys/mgmt/mcumgr/strzelnica/src/buttons.c
@@ -32,6 +32,16 @@
 #error "Unsupported board: BUTTON_CALL devicetree alias not found"
 #endif
 
+#define BUTTON_PRESS_SAMPLING_MS	20u
+#define BUTTON_DELAY_WORK_SAMPLING	K_MSEC(BUTTON_PRESS_SAMPLING_MS)
+#define BUTTON_PRESS_SHORT_MS		20u
+#define BUTTON_PRESS_LONG_MS		1000u
+#define BUTTON_PRESS_STUCK_MS		90000u
+
+#define BUTTON_PRESS_SHORT_CNT	(BUTTON_PRESS_SHORT_MS/BUTTON_PRESS_SAMPLING_MS)
+#define BUTTON_PRESS_LONG_CNT	(BUTTON_PRESS_LONG_MS/BUTTON_PRESS_SAMPLING_MS)
+#define BUTTON_PRESS_STUCTK_CNT	(BUTTON_PRESS_STUCK_MS/BUTTON_PRESS_SAMPLING_MS)
+
 LOG_MODULE_REGISTER(button);
 
 enum button_state {
@@ -43,8 +53,12 @@ enum button_state {
 };
 
 struct button_dev {
+	struct k_delayed_work work;
+
 	const struct device *dev;
 	gpio_pin_t pin;
+
+	uint16_t cnt;
 };
 
 static struct gpio_callback button_speed_cb_data;
@@ -53,17 +67,92 @@ static struct button_dev button_dev[BUTTON_NAME_MAX];
 static button_hander_t app_notify_cb;
 
 static void button_speed_pressed(const struct device *dev,
-	       			 struct gpio_callback *cb,
-		    		 uint32_t pins)
+				 struct gpio_callback *cb,
+				 uint32_t pins)
 {
-	app_notify_cb(BUTTON_NAME_SPEED, BUTTON_EVT_PRESSED_LONG);
+	LOG_INF("%s", __FUNCTION__);
+	button_dev[BUTTON_NAME_SPEED].cnt = 0;
+	/* disable interrupts and start filtering process */
+	gpio_pin_interrupt_configure(button_dev[BUTTON_NAME_SPEED].dev,
+				     button_dev[BUTTON_NAME_SPEED].pin,
+				     GPIO_INT_DISABLE);
+	k_delayed_work_submit(&button_dev[BUTTON_NAME_SPEED].work,
+			      BUTTON_DELAY_WORK_SAMPLING);
 }
 
 static void button_call_pressed(const struct device *dev,
-	       			 struct gpio_callback *cb,
-		    		 uint32_t pins)
+				struct gpio_callback *cb,
+				uint32_t pins)
 {
-	app_notify_cb(BUTTON_NAME_CALL, BUTTON_EVT_PRESSED_SHORT);
+	LOG_INF("%s", __FUNCTION__);
+	button_dev[BUTTON_NAME_CALL].cnt = 0;
+	/* disable interrupts and start filtering process */
+	k_delayed_work_submit(&button_dev[BUTTON_NAME_CALL].work,
+			      BUTTON_DELAY_WORK_SAMPLING);
+}
+
+/* Function for validating button speed */
+static void button_speed_filter(struct k_work *work)
+{
+	int state;
+
+	state = gpio_pin_get(button_dev[BUTTON_NAME_SPEED].dev,
+			     button_dev[BUTTON_NAME_SPEED].pin);
+
+	/* gpio_pin_get returns true if button is pressed */
+	if (!state) {
+		button_dev[BUTTON_NAME_SPEED].cnt++;
+		k_delayed_work_submit(&button_dev[BUTTON_NAME_SPEED].work,
+				      BUTTON_DELAY_WORK_SAMPLING);
+		return;
+	}
+
+	/* In case button is valid or stucked, interrupts must be enabled again
+	 * by the main app.
+	 */
+	if (button_dev[BUTTON_NAME_SPEED].cnt < BUTTON_PRESS_SHORT_CNT) {
+		/* Reject due to too short press */
+		return;
+	} else if (button_dev[BUTTON_NAME_SPEED].cnt < BUTTON_PRESS_LONG_CNT) {
+		app_notify_cb(BUTTON_NAME_SPEED, BUTTON_EVT_PRESSED_SHORT);
+	} else if (button_dev[BUTTON_NAME_SPEED].cnt < BUTTON_PRESS_STUCTK_CNT) {
+		app_notify_cb(BUTTON_NAME_SPEED, BUTTON_EVT_PRESSED_LONG);
+	} else {
+		app_notify_cb(BUTTON_NAME_SPEED, BUTTON_EVT_STUCKED);
+	}
+	button_enable(BUTTON_NAME_SPEED, false);
+}
+
+static void button_call_filter(struct k_work *work)
+{
+	int state;
+
+	state = gpio_pin_get(button_dev[BUTTON_NAME_CALL].dev,
+			     button_dev[BUTTON_NAME_CALL].pin);
+
+	/* gpio_pin_get returns true if button is pressed */
+	if (state) {
+		button_dev[BUTTON_NAME_CALL].cnt++;
+		k_delayed_work_submit(&button_dev[BUTTON_NAME_CALL].work,
+				      BUTTON_DELAY_WORK_SAMPLING);
+		return;
+	}
+
+	/* In case button is valid or stucked, interrupts must be enabled again
+	 * by the main app.
+	 * If the press is shorter than short press interrupts will be resumed
+	 */
+	if (button_dev[BUTTON_NAME_CALL].cnt < BUTTON_PRESS_SHORT_CNT) {
+		/* Reject due to too short press */
+		return;
+	} else if (button_dev[BUTTON_NAME_CALL].cnt < BUTTON_PRESS_LONG_CNT) {
+		app_notify_cb(BUTTON_NAME_CALL, BUTTON_EVT_PRESSED_SHORT);
+	} else if (button_dev[BUTTON_NAME_CALL].cnt < BUTTON_PRESS_STUCTK_CNT) {
+		app_notify_cb(BUTTON_NAME_CALL, BUTTON_EVT_PRESSED_LONG);
+	} else {
+		app_notify_cb(BUTTON_NAME_CALL, BUTTON_EVT_STUCKED);
+	}
+	button_enable(BUTTON_NAME_CALL, false);
 }
 
 int buttons_init(button_hander_t button_cb)
@@ -125,14 +214,28 @@ int buttons_init(button_hander_t button_cb)
 	gpio_add_callback(button_dev[BUTTON_NAME_CALL].dev,
 			  &button_call_cb_data);
 
+	k_delayed_work_init(&button_dev[BUTTON_NAME_SPEED].work,
+			    button_speed_filter);
+
+	k_delayed_work_init(&button_dev[BUTTON_NAME_CALL].work,
+			    button_call_filter);
+
 	return 0;
 }
 
 /* enable button interrupt */
-static int button_enable(enum button_name name, bool enable)
+int button_enable(enum button_name name, bool enable)
 {
 	int ret;
 	gpio_flags_t flags;
+
+	if (name >= BUTTON_NAME_MAX) {
+		LOG_WRN("%s: bad name:[%d]", __FUNCTION__, name);
+		return -EINVAL;
+	}
+
+	/* variable used for filtering */
+	button_dev[name].cnt = 0;
 
 	flags = enable ? GPIO_INT_EDGE_TO_ACTIVE : GPIO_INT_DISABLE;
 	ret = gpio_pin_interrupt_configure(button_dev[name].dev,
